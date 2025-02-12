12abf05ae0309b2c97d5baec17ef825750e1e979
Filippe Braga <filippeffx@hotmail.com>
2025-02-11 22:38:39 -0300

    hamb titles navbar component

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,7 +1,15 @@
 export function Navbar() {
   return (
     <div className="px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64">
-      Navbar
+      {/* LOGO */}
+      <div className="">
+        <img src="/logo.png" className="w-8 h-8" alt="" />
+        <span>devlog</span>
+      </div>
+      {/* MOBILE MENU */}
+      <div className="md:hidden">M</div>
+      {/* DESKTOP MENU */}
+      <div className="hidden md:flex">D</div>
     </div>
   )
 }
