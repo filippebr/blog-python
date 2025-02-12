72384f116db4c21d7884e374cccd7486c79391b2
Filippe Braga <filippeffx@hotmail.com>
2025-02-11 22:58:04 -0300

    style button tribar navbar component

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,4 +1,9 @@
+
 export function Navbar() {
+  // const {open, setOpen} = useState(false)
+
+
+
   return (
     <div className="w-full h-16 md:h-20 flex items-center justify-between">
       {/* LOGO */}
@@ -7,7 +12,11 @@ export function Navbar() {
         <span>devlog</span>
       </div>
       {/* MOBILE MENU */}
-      <div className="md:hidden">M</div>
+      <div className="md:hidden">
+        <div className="cursor-pointer text-4xl">
+          {open ? "X" : "≡­­­­"}
+        </div>
+      </div>
       {/* DESKTOP MENU */}
       <div className="hidden md:flex">D</div>
     </div>
