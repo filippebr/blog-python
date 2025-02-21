793ee7cfc849ac550114d506f6de5a43f61b171d
Filippe Braga <filippeffx@hotmail.com>
2025-02-21 00:47:14 -0300

    getting the value dynamic url from params route path

--- app/pages/RegisterPage.tsx
+++ app/pages/RegisterPage.tsx
@@ -1,4 +1,4 @@
-export function LoginPage() {
+export function RegisterPage() {
   return (
     <div className=""></div>
   )
--- app/routes/postlist.tsx
+++ app/routes/postlist.tsx
@@ -1,5 +1,19 @@
-export default function PostList() {
+import type { Route } from "./+types/postlist"
+
+export async function loader({ params }: Route.LoaderArgs) {
+  const postId = params.slug
+
+  return { postId }
+}
+
+export default function PostList({ loaderData }: Route.ComponentProps) {
   return (
-    <div className="">Post List</div>
+    <div className="font-bold">
+      <p>
+      Post Id: { loaderData.postId } 
+      </p>
+    </div>
   )
 }
+
+// https://medium.com/@nomannayeem/react-router-7-the-ultimate-guide-to-the-new-features-and-framework-capabilities-06e7f06981f6
