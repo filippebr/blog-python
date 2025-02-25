4a968ac63eb82119ab8263094a1bf9d02d6d993c
Filippe Braga <filippeffx@hotmail.com>
2025-02-25 20:10:32 -0300

    clerk setup

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { rootAuthLoader } from '@clerk/react-router/ssr.server'
 import {
   isRouteErrorResponse,
   Links,
@@ -9,6 +10,11 @@ import {
 import type { Route } from "./+types/root"
 import "./app.css"
 import MainLayout from "./layouts/mainlayout"
+
+// Clerk Setup
+export async function loader(args: Route.LoaderArgs) {
+  return rootAuthLoader(args)
+}
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,6 @@
 import { Main } from "~/welcome/app"
 import type { Route } from "./+types/home"
 
-// Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
-}
-
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Python Blog" },
