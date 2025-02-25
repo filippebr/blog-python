d113c5cf5bcdf73927cd462c511e73bd46e21640
Filippe Braga <filippeffx@hotmail.com>
2025-02-25 19:49:52 -0300

    env removed

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,13 @@
 import { Main } from "~/welcome/app"
 import type { Route } from "./+types/home"
 
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Python Blog" },
