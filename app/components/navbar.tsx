a10b9165f7d0b6a6c06f198c2a20057eaf672600
Filippe Braga <filippeffx@hotmail.com>
2025-02-25 19:27:06 -0300

    changed the anchor tag to link navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -23,24 +23,24 @@ export function Navbar() {
           font-medium text-lg absolute top-16 transition-all ease-in-out ${
           open ? "-right-0" : "-right-[100%]"
           }`}>
-          <a href="/">Home</a>
-          <a href="/">Trending</a>
-          <a href="/">Most Popular</a>
-          <a href="/">About</a>
-          <a href="/">
+          <Link to="/">Home</Link>
+          <Link to="/">Trending</Link>
+          <Link to="/">Most Popular</Link>
+          <Link to="/">About</Link>
+          <Link to="/">
             <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login 👋</button>
-          </a>
+          </Link>
         </div>
       </div>
       {/* DESKTOP MENU */}
       <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
-        <a href="/">Home</a>
-        <a href="/">Trending</a>
-        <a href="/">Most Popular</a>
-        <a href="/">About</a>
-        <a href="/">
+        <Link to="/">Home</Link>
+        <Link to="/">Trending</Link>
+        <Link to="/">Most Popular</Link>
+        <Link to="/">About</Link>
+        <Link to="/">
           <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login 👋</button>
-        </a>
+        </Link>
       </div>
     </div>
   )
