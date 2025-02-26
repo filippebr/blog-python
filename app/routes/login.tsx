5a9a7581d00c0c16e98db9d54bf47e4780408271
Filippe Braga <filippeffx@hotmail.com>
2025-02-26 15:48:49 -0300

    signin clerk element redirect register page

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/clerk-react"
 export default function Login() {
   return (
     <div className="flex items-center justify-center h-[calc(100vh-80px)]">
-      <SignIn />
+      <SignIn signUpUrl="/register" />
     </div>
   )
 }
