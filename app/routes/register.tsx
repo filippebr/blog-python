00b8efac60bd2a68deaf55b2a0dcd31c50466cb6
Filippe Braga <filippeffx@hotmail.com>
2025-02-26 16:08:13 -0300

    initial register layout signup clerk element

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -1,5 +1,9 @@
+import { SignUp } from "@clerk/clerk-react"
+
 export default function Register() {
   return (
-    <div className="">Register</div>
+    <div className="flex items-center justify-center h-[calc(100vh-80px)]">
+      <SignUp signInUrl="/login" />
+    </div>
   )
 }
