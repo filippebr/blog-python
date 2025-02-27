3270575e00b4d15beb1a8ce503df27e2078d9e6e
Filippe Braga <filippeffx@hotmail.com>
2025-02-27 20:46:34 -0300

    initial styles first others sections in featuredposts

--- app/components/featuredposts.tsx
+++ app/components/featuredposts.tsx
@@ -1,5 +1,14 @@
 export default function FeaturedPosts() {
   return (
-    <div className="">Featured Posts</div>
+    <div className="mt-8 flex flex-col lg:flex-row gap-8">
+      {/* First */}
+      <div className="w-full lg:w-1/2 flex flex-col gap-4">
+        
+      </div>
+      {/* Others */}
+      <div className="w-full lg:w-1/2 flex flex-col gap-4">
+        
+      </div>
+    </div>
   )
 }
