44734f08a70875a2c750de342a2b22ef3ff0e3e5
Filippe Braga <filippeffx@hotmail.com>
2025-02-27 18:18:05 -0300

    initial style breadcrumb home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router"
 import type { Route } from "./+types/home"
 
 export function meta({}: Route.MetaArgs) {
@@ -12,6 +13,11 @@ export default function Home() {
     <div className="mt-4 flex flex-col gap-4">
       Home Page
       {/* BREADCRUMB */}
+      <div className="flex gap-4">
+        <Link to="/">Home</Link>
+        <span>•</span>
+        <span className="text-blue-800">Blogs and Articles</span>
+      </div>
       {/* INTRODUCTION */}
       {/* FEATURED POST */}
       {/* POST LIST */}
