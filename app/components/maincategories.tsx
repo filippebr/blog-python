02e48f9039d8b6f38bb58e62b17323be1e456804
Filippe Braga <filippeffx@hotmail.com>
2025-02-27 20:30:03 -0300

    links maincategories

--- app/components/maincategories.tsx
+++ app/components/maincategories.tsx
@@ -7,8 +7,23 @@ export default function MainCategories() {
     ">
       {/* links */}
       <div className="flex-1 flex items-center justify-between flex-wrap">
-        <Link to="">
+        <Link to="/posts" className="bg-blue-800 text-white rounded-full px-4 py-2">
           All Posts
+        </Link>
+        <Link to="/posts?cat=web-design" className="hover:bg-blue-50 rounded-full px-4 py-2">
+          Web Design
+        </Link>
+        <Link to="/posts?cat=development" className="hover:bg-blue-50 rounded-full px-4 py-2">
+          Development
+        </Link>
+        <Link to="/posts?cat=databases" className="hover:bg-blue-50 rounded-full px-4 py-2">
+          Databases
+        </Link>
+        <Link to="/posts?cat=seo" className="hover:bg-blue-50 rounded-full px-4 py-2">
+          Search Engines
+        </Link>
+        <Link to="/posts?cat=marketing" className="hover:bg-blue-50 rounded-full px-4 py-2">
+          Marketing
         </Link>
       </div>
       {/* search */}
