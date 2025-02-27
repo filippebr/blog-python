2a86e7ab5bff4fee8f31341146885e34051f26f9
Filippe Braga <filippeffx@hotmail.com>
2025-02-27 20:24:20 -0300

    initial styles links maincategories

--- app/components/maincategories.tsx
+++ app/components/maincategories.tsx
@@ -1,10 +1,18 @@
+import { Link } from "react-router"
+
 export default function MainCategories() {
   return (
     <div className="
       hidden md:flex bg-white rounded-3xl xl:rounded-full p-4 shadow-lg items-center justify-center gap-8
     ">
       {/* links */}
-      
+      <div className="flex-1 flex items-center justify-between flex-wrap">
+        <Link to="">
+          All Posts
+        </Link>
+      </div>
+      {/* search */}
+      <div className="">Search</div>
     </div>
   )
 }
