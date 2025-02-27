59c62b781e885baa539d5146022911ecbbc86890
Filippe Braga <filippeffx@hotmail.com>
2025-02-27 18:26:10 -0300

    text in introduction home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -25,7 +25,7 @@ export default function Home() {
           <h1 className="text-gray-800 text-2xl md:text-5xl lg:text-6xl font-bold">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
           </h1>
-          <p>
+          <p className="mt-8 text-md md:text-xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod itaque quis deserunt, 
             nesciunt saepe unde adipisci.
           </p>
