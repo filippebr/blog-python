aad8840965a4fa27005a08619ad1020b18943e33
Filippe Braga <filippeffx@hotmail.com>
2025-03-01 20:59:59 -0300

    styles author details singlepost

--- app/routes/singlepost.tsx
+++ app/routes/singlepost.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router"
 import Image from "~/components/image"
 
 export default function SinglePost() {
@@ -9,6 +10,13 @@ export default function SinglePost() {
           <h1>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet porro vitae consequuntur.
           </h1>
+          <div className="flex items-center gap-2 text-gray-400 text-sm">
+            <span>Written by</span>
+            <Link to="">John Doe</Link>
+            <span>on</span>
+            <Link to="">Web Design</Link>
+            <span>2 days ago</span>
+          </div>          
         </div>
         <div className="hidden lg:block w-2/5">
           <Image src="postImg.jpeg" alt="single post" />
