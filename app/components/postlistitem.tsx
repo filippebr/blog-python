c7c716612a18c04024362ac4ba4b2b1e80185680
Filippe Braga <filippeffx@hotmail.com>
2025-03-01 19:51:00 -0300

    resize details author postlistitem

--- app/components/postlistitem.tsx
+++ app/components/postlistitem.tsx
@@ -5,11 +5,11 @@ export default function PostListItem() {
   return (
     <div className="flex flex-col xl:flex-row gap-8">
       {/* image */}
-      <div className="md:hidden xl:block">
+      <div className="md:hidden xl:block xl:w-1/3">
         <Image src="postImg.jpeg" alt="post image" className="rounded-2xl object-cover" />
       </div>
       {/* details */}
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 xl:w-2/3">
         <Link to="/test" className="text-4xl font-semibold">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis vero quia, illum dolorem.
         </Link>
@@ -20,12 +20,12 @@ export default function PostListItem() {
           <Link to="" className="text-blue-800">Web Design</Link>
           <span>2 days ago</span>
         </div>  
-        <p className="">
+        <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia eaque tempora quam voluptate? 
           Iste numquam voluptates veritatis molestiae officiis fugiat optio laudantium assumenda consectetur 
           ipsam? A sit enim nemo dicta.
         </p>
-        <Link to="/test">Read More</Link>
+        <Link to="/test" className="underline text-blue-800 text-sm">Read More</Link>
       </div>
       
     </div>
