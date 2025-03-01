c70d6483e1ac0965b6d0dc98a130ad670b728dd3
Filippe Braga <filippeffx@hotmail.com>
2025-03-01 20:52:48 -0300

    title style singlepost

--- app/routes/singlepost.tsx
+++ app/routes/singlepost.tsx
@@ -1,5 +1,22 @@
+import Image from "~/components/image"
+
 export default function SinglePost() {
   return (
-    <div className="">Single Post</div>
+    <div className="flex flex-col gap-8">
+      {/* detail */}
+      <div className="flex gap-8">
+        <div className="lg:w-3/5 flex flex-col gap-8">
+          <h1>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet porro vitae consequuntur.
+          </h1>
+        </div>
+        <div className="hidden lg:block w-2/5">
+          <Image src="postImg.jpeg" alt="single post" />
+        </div>
+        <div className=""></div>
+      </div>
+      {/* content */}
+
+    </div>
   )
 }
