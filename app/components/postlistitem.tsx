6e35f12f4e55b0febe768cc20ea7f640aa5f992e
Filippe Braga <filippeffx@hotmail.com>
2025-03-01 19:04:06 -0300

    styles details author postlistitem

--- app/components/postlistitem.tsx
+++ app/components/postlistitem.tsx
@@ -14,7 +14,13 @@ export default function PostListItem() {
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis vero quia, illum dolorem.
         </Link>
       </div>
-      <div className=""></div>
+      <div className="flex items-center gap-2 text-gray-400 text-sm">
+        <span>Written by</span>
+        <Link to="">John Doe</Link>
+        <span>on</span>
+        <Link to="">Web Design</Link>
+        <span>2 days ago</span>
+      </div>
     </div>
   )
 }
