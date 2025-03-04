881de3de51f6470c5e19ae4cc89cff7668c332f7
Filippe Braga <filippeffx@hotmail.com>
2025-03-04 17:40:26 -0300

    styles titles sidemenu

--- app/components/sidemenu.tsx
+++ app/components/sidemenu.tsx
@@ -1,5 +1,12 @@
+import Search from "./search"
+
 export default function SideMenu() {
   return (
-    <div className="">Side Menu</div>
+    <div className="px-4 h-max sticky top-8 ">
+      <h1 className="mb-4 text-sm font-medium">Search</h1>
+      <Search />
+      <h1 className="mt-8 mb-4 text-sm font-medium">Filters</h1>
+      <h1 className="mt-8 mb-4 text-sm font-medium">Categories</h1>
+    </div>
   )
 }
