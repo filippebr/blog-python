0b6a68d50c8653220ba747f61907165637b097fe
Filippe Braga <filippeffx@hotmail.com>
2025-03-03 21:32:34 -0300

    elements select form write

--- app/routes/write.tsx
+++ app/routes/write.tsx
@@ -8,7 +8,28 @@ export default function Write() {
     return <div className="">Loading...</div>
   }
 
+  if(isLoaded && !isSignedIn) {
+    return <div className="">You should login!</div>
+  }
+
   return (
-    <div className="">Write</div>
+    <div className="">
+      <h1>Create a New Post</h1>
+      <form action="">
+        <button>Add a cover image</button>
+        <input type="text" placeholder="My Awesome Story" />
+        <div className="">
+          <label htmlFor="">Choose a category:</label>
+          <select name="cat" id="">
+            <option value="general">General</option>
+            <option value="web-design">Web Design</option>
+            <option value="development">Development</option>
+            <option value="databases">Databases</option>
+            <option value="seo">Search Engines</option>
+            <option value="marketing">Marketing</option>
+          </select>
+        </div>
+      </form>
+    </div>
   )
 }
