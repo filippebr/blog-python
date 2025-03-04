92f3f0e8610a1457185a67c979ca3c84f7d2da18
Filippe Braga <filippeffx@hotmail.com>
2025-03-04 17:36:19 -0300

    show or hidden sidemenu postlistpage

--- app/routes/postlistpage.tsx
+++ app/routes/postlistpage.tsx
@@ -22,11 +22,13 @@ export default function PostListPage({ loaderData }: Route.ComponentProps) {
       <button onClick={() => setOpen(prev => !prev)} className="bg-blue-800 text-sm text-white px-4 py-2 rounded-2xl mb-4 md:hidden">
         {open ? "Close" : "Filter or Search"}
       </button>
-      <div className="flex gap-8">
+      <div className="flex flex-col-reverse gap-8 md:flex-row">
         <div className="">
           <PostList /> 
         </div>
-        <div className=""><SideMenu /></div>
+        <div className={`${open ? "block" : "hidden"} md:block`}>
+          <SideMenu />
+        </div>
       </div>
     </div>
   )
