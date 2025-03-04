ac9cc0b9687c844105544965ba87464337c048cc
Filippe Braga <filippeffx@hotmail.com>
2025-03-04 17:51:39 -0300

    input radio check filter sidemenu

--- app/components/sidemenu.tsx
+++ app/components/sidemenu.tsx
@@ -7,6 +7,16 @@ export default function SideMenu() {
       <h1 className="mb-4 text-sm font-medium">Search</h1>
       <Search />
       <h1 className="mt-8 mb-4 text-sm font-medium">Filters</h1>
+      <div className="flex flex-col gap-2 text-sm ">
+        <label htmlFor="" className="flex items-center gap-2 cursor-pointer">
+          <input 
+            type="radio" 
+            name="sort" 
+            value="newest" 
+            className="appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm checked:bg-blue-800" 
+          />
+        </label>
+      </div>
       <h1 className="mt-8 mb-4 text-sm font-medium">Categories</h1>
       <div className="flex flex-col gap-2 text-sm">
         <Link to="/posts" className="underline">All</Link>
