8ec1117903556c1cd38991a82128a4eea13e15ec
Filippe Braga <filippeffx@hotmail.com>
2025-03-04 17:45:01 -0300

    categories links sidemenu

--- app/components/sidemenu.tsx
+++ app/components/sidemenu.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router"
 import Search from "./search"
 
 export default function SideMenu() {
@@ -7,6 +8,14 @@ export default function SideMenu() {
       <Search />
       <h1 className="mt-8 mb-4 text-sm font-medium">Filters</h1>
       <h1 className="mt-8 mb-4 text-sm font-medium">Categories</h1>
+      <div className="flex flex-col gap-2 text-sm">
+        <Link to="/posts" className="underline">All</Link>
+        <Link to="/posts?cat=web-design" className="underline">Web Design</Link>
+        <Link to="/posts?cat=development" className="underline">Development</Link>
+        <Link to="/posts?cat=databases" className="underline">Databases</Link>
+        <Link to="/posts?cat=seo" className="underline">Search Engines</Link>
+        <Link to="/posts?cat=marketing" className="underline">Marketing</Link>
+      </div>
     </div>
   )
 }
