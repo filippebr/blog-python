b38539e1718af572e28cc1ae34cba1a2be020923
Filippe Braga <filippeffx@hotmail.com>
2025-03-03 21:25:49 -0300

    loading useuser write component

--- app/routes/write.tsx
+++ app/routes/write.tsx
@@ -1,4 +1,13 @@
+import { useUser } from "@clerk/clerk-react"
+
 export default function Write() {
+
+  const {isLoaded, isSignedIn} = useUser()
+
+  if(!isLoaded) {
+    return <div className="">Loading...</div>
+  }
+
   return (
     <div className="">Write</div>
   )
