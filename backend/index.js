0ef05dd8d651e5d8ec449be51e97d721904bb15b
Filippe Braga <filippeffx@hotmail.com>
2025-03-06 20:47:18 -0300

    initial api request server

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ import express from "express"
 
 const app = express()
 
+app.get("/test", (req, res) => {
+  res.status(200).send("it works!")
+})
+
 app.listen(3000, () => {
   console.log("Server is running!")
 })
