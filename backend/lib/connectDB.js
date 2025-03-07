1e3035d655ffbbb73642091613032a969a15d5b3
Filippe Braga <filippeffx@hotmail.com>
2025-03-06 23:19:23 -0300

    user schema mongoose

--- backend/lib/connectDB.js
+++ backend/lib/connectDB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO)
-    console.log("Mongo DB is connect")
+    console.log("Mongo DB is connected")
   } catch(err) {
     console.log(err)
   }
