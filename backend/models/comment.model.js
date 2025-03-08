b4eea38f29519b4d04419f05927de272357bb0b1
Filippe Braga <filippeffx@hotmail.com>
2025-03-08 18:07:23 -0300

    comment schema backend

--- backend/models/comment.model.js
+++ backend/models/comment.model.js
@@ -1,23 +1,21 @@
 import mongoose, { Schema } from "mongoose"
 
-const commentSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  img: {
-    type: String,
-  },
-  savedPosts: {
-    type: [String],
-    default: [],
-  }
+const commentSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    post: {
+      type: Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },  
 },{timestamps: true})
 
 export default mongoose.model("Comment", commentSchema)
