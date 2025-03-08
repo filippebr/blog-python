746d3ee82dd32309a3e0ed3462026d791b23a817
Filippe Braga <filippeffx@hotmail.com>
2025-03-08 20:45:14 -0300

    new error handler express 5

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,8 +1,8 @@
 import Post from "../models/post.model.js"
 
-export const getPosts = async (req, res) => {
-  const posts = await Post.find()
-  res.status(200).json(posts)
+export const getPosts = async (req, res, next) => {
+    const posts = await Post.find()
+    res.status(200).json(posts)
 }
 
 export const getPost = async (req, res) => {
