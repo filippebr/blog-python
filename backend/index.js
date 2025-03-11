78ab3cc90bc18f9f6ff3cc68731f1e8a2b20a7c0
Filippe Braga <filippeffx@hotmail.com>
2025-03-11 17:55:49 -0300

    auth-state route clerkmiddleware

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+import { clerkMiddleware } from '@clerk/express'
 import express from "express"
 import connectDB from "./lib/connectDB.js"
 import commentRouter from "./routes/comment.route.js"
@@ -6,8 +7,14 @@ import userRouter from "./routes/user.route.js"
 import webhookRouter from "./routes/webhook.route.js"
 
 const app = express()
+app.use(clerkMiddleware())
 app.use("/webhooks", webhookRouter)
 app.use(express.json())
+
+app.get("/auth-state", (req, res) => {
+  const authState = req.auth
+  res.json(authState)
+})
 
 app.use("/users", userRouter)
 app.use("/posts", postRouter)
