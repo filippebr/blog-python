54afcb101f9c8c653e6b15d00c1769a88ac14193
Filippe Braga <filippeffx@hotmail.com>
2025-03-15 19:31:05 -0300

    axios installed

--- client/app/components/postlist.tsx
+++ client/app/components/postlist.tsx
@@ -1,6 +1,19 @@
+import { useQuery } from "@tanstack/react-query"
 import PostListItem from "./postlistitem"
 
 export default function PostList() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () =>
+      fetch('https://api.github.com/repos/TanStack/query').then((res) =>
+        res.json(),
+      ),
+  })
+
+  if (isPending) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.message
+
   return (
     <div className="flex flex-col gap-12 mb-8">
       <PostListItem />
