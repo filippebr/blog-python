582bec6680ae49a3872431b61b1b801dfea2e6e6
Filippe Braga <filippeffx@hotmail.com>
2025-03-15 19:41:35 -0300

    cors installed

--- client/app/components/postlist.tsx
+++ client/app/components/postlist.tsx
@@ -1,18 +1,24 @@
 import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
 import PostListItem from "./postlistitem"
+
+const fetchPosts = async () => {
+  const res = await axios.get(`${import.meta.env.VITE_API_URL}/posts`)
+  
+  return res.data
+}
 
 export default function PostList() {
   const { isPending, error, data } = useQuery({
     queryKey: ['repoData'],
-    queryFn: () =>
-      fetch('https://api.github.com/repos/TanStack/query').then((res) =>
-        res.json(),
-      ),
+    queryFn: () => fetchPosts(),
   })
 
   if (isPending) return 'Loading...'
 
   if (error) return 'An error has occurred: ' + error.message
+
+  console.log(data)
 
   return (
     <div className="flex flex-col gap-12 mb-8">
