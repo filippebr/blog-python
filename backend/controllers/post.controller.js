c93e2bf146d57d6db438caf302f864075bbc08f2
Filippe Braga <filippeffx@hotmail.com>
2025-03-18 20:19:16 -0300

    imagekit controller

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,3 +1,4 @@
+import ImageKit from "imagekit"
 import Post from "../models/post.model.js"
 import User from "../models/user.model.js"
 
@@ -69,4 +70,15 @@ export const deletePost = async (req, res) => {
   })
 
   res.status(200).json("Post has been deleted")
+}
+
+const imagekit = new ImageKit({
+  urlEndpoint: process.env.IK_URL_ENDPOINT,
+  publicKey: process.env.IK_PUBLIC_KEY,
+  urlEndpoint: process.env.IK_PRIVATE_KEY
+})
+
+export const uploadAuth = async(req, res) => {
+  const result = imagekit.getAuthenticationParameters()
+  res.send(result)
 }
