f80fea891f21ef06fe6c0685c773b0185ec6d803
Filippe Braga <filippeffx@hotmail.com>
2025-03-25 22:05:46 -0300

    comment routes cr*d

--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -2,4 +2,8 @@ import express from 'express'
 
 const router = express.Router()
 
+router.get("/:postId", getPostComments)
+router.post("/:postId", addComment)
+router.delete("/:id", deleteComment)
+
 export default router
