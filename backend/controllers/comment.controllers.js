c865625bb7b2223b05662bed5915b3a9deca3057
Filippe Braga <filippeffx@hotmail.com>
2025-03-26 15:37:12 -0300

    initial deletecomment controller

--- backend/controllers/comment.controllers.js
+++ backend/controllers/comment.controllers.js
@@ -31,5 +31,19 @@ export const addComment = async(req, res) => {
 }
 
 export const deleteComment = async(req, res) => {
+  const clerkUserId = req.auth.userId
+  const id = req.params.id
 
+  if (!clerkUserId) {
+    return res.status(401).json("Not authenticated!")
+  }
+
+  const user = User.findOne({clerkUserId})
+
+  const deletedComment = await Comment.findOneAndDelete({
+    _id: id, 
+    user: user._id
+  })
+
+  
 }
