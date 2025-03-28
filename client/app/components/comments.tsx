4e1796d85adfd70ad0b69ccf34451dcd6278fb35
Filippe Braga <filippeffx@hotmail.com>
2025-03-27 22:55:23 -0300

    textarea name desc comments

--- client/app/components/comments.tsx
+++ client/app/components/comments.tsx
@@ -68,11 +68,11 @@ export default function Comments({ postId }: CommentProps) {
     const formData = new FormData(event.currentTarget)
 
     const data: CommentProps = {
-      postId: formData.get("postId")?.toString() ?? "",
-      desc: formData.get("desc")?.toString() ?? "",
+      postId,
+      desc: formData.get("desc")?.toString() || "",
     }
 
-    console.log(data)
+    console.log("Sending data:", data)
 
     mutation.mutate(data)
   }
@@ -81,7 +81,11 @@ export default function Comments({ postId }: CommentProps) {
     <div className="flex flex-col gap-8 lg:w-3/5 mb-12">
       <h1 className="text-xl text-gray-500 underline">Comments</h1>
       <form onSubmit={handleSubmit} className="flex items-center justify-between gap-8 w-full">
-        <textarea placeholder="Write a comment..." className="w-full p-4 rounded-xl" />
+        <textarea 
+          name="desc" 
+          placeholder="Write a comment..." 
+          className="w-full p-4 rounded-xl" 
+        />
         <button className="cursor-pointer bg-blue-800 px-4 py-3 text-white font-medium rounded-xl">Send</button>
       </form>
       {data.map((comment) => 
