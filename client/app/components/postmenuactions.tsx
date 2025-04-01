64b9bdb6fa1c3049e1b39a0d72404fe97cbcacd5
Filippe Braga <filippeffx@hotmail.com>
2025-04-01 18:11:46 -0300

    ispending ternary options postmenuactions

--- client/app/components/postmenuactions.tsx
+++ client/app/components/postmenuactions.tsx
@@ -28,7 +28,11 @@ export default function PostMenuActions({ post }: PostListItemProps ) {
   return (
     <div className="">
       <h1 className="mt-8 mb-4 text-sm font-medium">Actions</h1>
-      <div className="flex items-center gap-2 py-2 text-sm cursor-pointer">
+      {isPending ?  
+        "Loading..." 
+        : error 
+        ? "Saved Posts fetching failed!" 
+        : <div className="flex items-center gap-2 py-2 text-sm cursor-pointer">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 48 48"
@@ -42,7 +46,7 @@ export default function PostMenuActions({ post }: PostListItemProps ) {
           />
         </svg>
         <span>Save this post</span>
-      </div>
+      </div>}
       {user && (post.user.username === user.username) && (
         <div className="flex items-center gap-2 py-2 text-sm cursor-pointer">
           <svg 
