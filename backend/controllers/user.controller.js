0c8653cb7fbafc8d3c7f7d9193b24f0785d7d65f
Filippe Braga <filippeffx@hotmail.com>
2025-04-01 17:57:43 -0300

    savedposts controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -11,4 +11,29 @@ export const getUserSavedPosts = async(req, res) => {
 
   res.status(200).json(user.savedPosts)
 } 
-export const savePost = async(req, res) => {} 
+
+export const savePost = async(req, res) => {
+
+  const clerkId = req.auth.userId
+  const postId = req.body.userId
+
+  if (!clerkId) {
+    return res.status(401).json("Not authenticated!")
+  }
+
+  const user = await User.findOne({ clerkUserId })
+
+  const isSaved = user.savedPosts.some(p => p === postId)
+
+  if (!isSaved) {
+    await User.findByIdAndUpdate(user._id, {
+      $push: { savedPosts: postId },
+    })
+  } else {
+    await User.findByIdAndUpdate(user._id, {
+      $pull: { savedPosts: postId },
+    })
+  }
+
+  res.status(200).json(isSaved ? "Post unsaved" : "Post saved")
+} 
